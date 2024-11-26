e825f6216ada47512b55ba742e6c2aea3f92789d
Baotran <baotran@BaoTran>
2024-11-26 10:44:29 +0700

    v1.0.2

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit67eed3b02ae319d9743b3cdf21cb7371
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit67eed3b02ae319d9743b3cdf21cb7371::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
